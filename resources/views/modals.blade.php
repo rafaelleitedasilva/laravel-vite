06f15a72a12ba457a31bb8d74c8be9e6a5b6a0cd
Rafael <rafael.leite.14@hotmail.com>
2023-10-04 05:45:12 -0300

    Feat Modal: Making Work's modal

---
+++ resources/views/modals.blade.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="fullscreenModalFluit" tabindex="-1" role="dialog" aria-labelledby="fullscreenModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-fullscreen">
+        <div class="modal-content gallery-area">
+            <!-- Conteúdo do modal aqui -->
+            <div class="modal-header border-0">
+                <h5 class="modal-title text-white" id="fullscreenModalLabel">
+                    Fluit
+                </h5>
+                <button type="button" class="gallery-area border-0" data-bs-dismiss="modal" aria-label="Fechar">
+                    <img style="width: 30px;" src="{{Vite::asset('resources/images/icones/times.svg')}}" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
