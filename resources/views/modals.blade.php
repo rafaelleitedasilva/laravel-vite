8ca8509736dfec0a5f17cb5ebe7cd0841f9f6212
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2023-10-04 08:36:54 -0300

    Feat Index: Adjusting Index´s links and style.scss

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,15 +1,18 @@
 <div class="modal fade" id="fullscreenModalFluit" tabindex="-1" role="dialog" aria-labelledby="fullscreenModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-fullscreen">
-        <div class="modal-content gallery-area">
+        <div class="modal-content gallery-area text-white">
             <!-- Conteúdo do modal aqui -->
             <div class="modal-header border-0">
-                <h5 class="modal-title text-white" id="fullscreenModalLabel">
+                <h5 class="modal-title " id="fullscreenModalLabel">
                     Fluit
                 </h5>
                 <button type="button" class="gallery-area border-0" data-bs-dismiss="modal" aria-label="Fechar">
                     <img style="width: 30px;" src="{{Vite::asset('resources/images/icones/times.svg')}}" alt="">
                 </button>
             </div>
+            <p class="text-white">
+                O projeto Fluit idealizado pela empresa ChoppUp e gerido pela Kasi Consultoria de TI
+            </p>
         </div>
     </div>
 </div>
