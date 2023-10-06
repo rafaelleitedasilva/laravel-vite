cb13df65987c782555c1e8a4c650eb11df8421ce
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2023-10-06 08:57:07 -0300

    Feat Markdown: Generating and Updating Markdown Mail

--- resources/views/emails/portfolio.blade.php
+++ resources/views/emails/portfolio.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Email de Contato - Rafael Leite da Silva</title>
-</head>
-<body>
-    <h2>Informações do Contato</h2>
-    <p><strong>Nome:</strong> {{ $name }}</p>
-    <p><strong>Email:</strong> {{ $email }}</p>
-    <p><strong>Mensagem:</strong></p>
-    <p>{!! $emailMessage !!}</p>
-</body>
-</html>
+<x-mail::message>
+    {{ $name }},
+    <br>
+    {!! $emailMessage !!}
+    <hr>
+    Email para contato: {{ $email }}
+</x-mail::message>
