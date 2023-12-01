90ecf1a7bb68138879168f6fbfd45f061eabec68
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2023-12-01 07:43:25 -0300

    Feat: Copyright Gifs

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
     @vite(['resources/css/app.css', 'resources/scss/style.scss', 'resources/js/app.js'])
 </head>
 
