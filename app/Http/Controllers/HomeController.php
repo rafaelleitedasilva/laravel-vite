b4cb84f62b4699c60211e77b32cab72d3712930e
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2024-12-27 22:15:51 -0300

    Feat HomeController: Creating a funcion email to send the emails

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\portfolio;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $projetos = \App\Models\Projects::orderBy('name','ASC')->get()->toArray();
 
         foreach($projetos as $index=>$value){
@@ -24,5 +29,47 @@ class HomeController extends Controller
 
 
         return view('index')->with("projetos",$projetos);
+    }
+
+    public function email(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+            'context' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            // Se a validação passar, continue o processo
+            $nome = $request->name;
+            $email = $request->email;
+            $mensagem = $request->message;
+            $context = $request->context;
+
+            $message = new portfolio($nome, $email, $mensagem, $context);
+            $message->to('rafael.leite.14@hotmail.com');
+
+            Mail::send($message);
+            Session::flash('message', $validator->errors());
+
+            return redirect('/home#contato');
+        } else {
+            // Se a validação passar, continue o processo
+            $nome = $request->name;
+            $email = $request->email;
+            $mensagem = $request->message;
+            $context = $request->context;
+
+            $message = new portfolio($nome, $email, $mensagem, $context);
+            $message->to('rafael.leite.14@hotmail.com');
+
+            Mail::send($message);
+            Session::flash('message', 'Sua mensagem foi enviada, obrigado por entrar em contato!');
+
+            return redirect('/home#contato');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Mail\portfolio;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Session;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,48 +20,7 @@ Route::get('/', function () {
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::post('/email', function (Request $request) {
-    $rules = [
-        'name' => 'required',
-        'email' => 'required|email',
-        'message' => 'required',
-        'context' => 'required',
-    ];
-
-    $validator = Validator::make($request->all(), $rules);
-
-    if ($validator->fails()) {
-        // Se a validação passar, continue o processo
-        $nome = $request->name;
-        $email = $request->email;
-        $mensagem = $request->message;
-        $context = $request->context;
-
-        $message = new portfolio($nome, $email, $mensagem, $context);
-        $message->to('rafael.leite.14@hotmail.com');
-
-        Mail::send($message);
-        Session::flash('message', $validator->errors());
-
-        return redirect('/home#contato');
-    } else {
-        // Se a validação passar, continue o processo
-        $nome = $request->name;
-        $email = $request->email;
-        $mensagem = $request->message;
-        $context = $request->context;
-
-        $message = new portfolio($nome, $email, $mensagem, $context);
-        $message->to('rafael.leite.14@hotmail.com');
-
-        Mail::send($message);
-        Session::flash('message', 'Sua mensagem foi enviada, obrigado por entrar em contato!');
-
-        return redirect('/home#contato');
-    }
-
-
-})->name('email');
+Route::post('/email', [\App\Http\Controllers\HomeController::class, 'email'])->name('email');
 
 // Route::get('/construction', function () {
 //     return view('construction');
