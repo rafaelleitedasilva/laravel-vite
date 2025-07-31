e9d1a6d9f8850bfd531b89eb7118f5742438e08c
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2025-07-31 16:58:15 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,45 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-            'resources/css/app.css',
-            'resources/scss/style.scss',
-            'resources/js/app.js',
-        ],refresh: true
-        })
+                'resources/css/app.css',
+                'resources/scss/style.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
 
-    // css: {
-    //     postcss: null,
-    //     preprocessorOptions: {
-    //         scss: {
-    //             additionalData: `
-    //                 @import "./resources/scss/variables";
-    //                 @import "./resources/scss/color";
-    //             `,
-    //         },
-    //     },
-    // },
-
-    resolve:{
-        alias:{
-            "@":"/resources/js",
-        }
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+        },
     },
+
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                style: 'resources/scss/style.scss',
+            },
+        },
+        emptyOutDir: true,
+    },
+
     server: {
-        https: false, // Use HTTPS apenas em produção
-        host: true, // Use diferentes hosts para dev e prod
+        https: false,
+        host: true,
         strictPort: true,
-        port: 3009, // Use diferentes portas para dev e prod
-        hmr: { host: 'localhost', protocol: 'ws'},
+        port: 3009,
+        hmr: { host: 'localhost', protocol: 'ws' },
         watch: {
             usePolling: true,
-        }
-    }
+        },
+    },
 });
