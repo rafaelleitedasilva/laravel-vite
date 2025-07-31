51a1ed07c2fd8546effe3e8dc0bb5f3f763459c1
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2025-07-31 17:02:06 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
 /*
 |--------------------------------------------------------------------------
