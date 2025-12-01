528f83e068a62b1e34d47c97d09cc89cb01b63a9
Rafael <69431917+rafaelleitedasilva@users.noreply.github.com>
2025-12-01 07:43:25 -0300

    Initialize Laravel application in index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
